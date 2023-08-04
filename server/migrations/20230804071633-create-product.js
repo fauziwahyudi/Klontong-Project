60a28759cad2d907883e1d617a1e13dceb721c4a
fauziwahyudi <fauziwahyudi12@gmail.com>
2023-08-04 14:34:48 +0700

    Setting confiq.json

---
+++ server/migrations/20230804071633-create-product.js
@@ -0,0 +1,81 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references : {
+          model: "Categories",
+          key : "id"
+        }
+      },
+      categoryName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      sku: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      weight: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      width: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      length: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      height: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      authorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references : {
+          model: "Users",
+          key : "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
